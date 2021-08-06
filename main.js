cb7932785b9d071ab5530dc07627235f82f4b8f9
Coolcheese75 <77335832+Coolcheese75@users.noreply.github.com>
2021-08-06 20:28:44 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+var images = [
+    "myselfie (2).png",
+    "myselfie (4).png",
+    "myselfie (6).png",
+    "myselfie (8).png",
+    "WHITE BREAD EATIN BOI.jpg"
+    
+  ];
+  var i = 0;
+  function nextslide() {
+    
+    if(i == 9)
+      {
+        i=0;
+      }
+    
+   document.getElementById("album").src = myselfie.png;
+    i++;
+  }
